0d63733d9deedd7aa385e64418510483b329c44f
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-06-16 22:56:19 +0900

    feat: Object 클래스 상속 구조와 toString 기본 동작 연습

---
+++ src/lang/object/ObjectMain.java
@@ -0,0 +1,18 @@
+package lang.object;
+
+public class ObjectMain {
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.childMethod();
+        child.parentMethod();
+
+        String string = child.toString();
+        System.out.println(string);
+    }
+}
+
+/*
+Child.childMethod
+Parent.parentMethod
+lang.object.Child@3f99bd52
+*/
