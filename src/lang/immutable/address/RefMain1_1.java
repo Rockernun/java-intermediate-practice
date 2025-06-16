f0bbda1bcca7120adba632fb57a4336c9d4cae10
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-06-16 23:25:36 +0900

    feat: Address 인스턴스를 별도로 생성하여 참조형 Side Effect 방지

--- src/lang/immutable/address/RefMain1_1.java
+++ src/lang/immutable/address/RefMain1_1.java
@@ -3,9 +3,9 @@ package lang.immutable.address;
 public class RefMain1_1 {
     public static void main(String[] args) {
 
-        // 참조형 변수는 하나의 인스턴스를 공유할 수 있다.
+        // 이런 식으로 처음부터 다른 인스턴스를 참조하도록
         Address address1 = new Address("서울");
-        Address address2 = address1;
+        Address address2 = new Address("서울");
 
         System.out.println("address1 = " + address1);
         System.out.println("address2 = " + address2);
@@ -19,6 +19,6 @@ public class RefMain1_1 {
 /*
 address1 = Address{value='서울'}
 address2 = Address{value='서울'}
-address1 = Address{value='부산'}
+address1 = Address{value='서울'}
 address2 = Address{value='부산'}
 */
