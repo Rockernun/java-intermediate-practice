5526f5434dc86cf106709288c29eee80ee305ab6
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-06-17 13:14:26 +0900

    feat: 문자열 분할 및 조합 메서드 (split, join) 연습 코드 작성

---
+++ src/lang/string/method/SpringSplitJoinMain.java
@@ -0,0 +1,30 @@
+package lang.string.method;
+
+public class SpringSplitJoinMain {
+    public static void main(String[] args) {
+
+        String str = "Grape,Strawberry,Melon";
+
+        // split
+        String[] splitStr = str.split(",");
+        for (String s : splitStr) {
+            System.out.println(s);
+        }
+
+        // join
+        String joinedStr = String.join("-", "A", "B", "C");
+        System.out.println("연결된 문자열: " + joinedStr);
+
+        // 문자열 배열 연결
+        String result = String.join("-", splitStr);
+        System.out.println("result = " + result);
+    }
+}
+
+/*
+Grape
+Strawberry
+Melon
+연결된 문자열: A-B-C
+result = Grape-Strawberry-Melon
+*/
