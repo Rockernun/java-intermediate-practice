5fb2dfe31c90c96267ff83004ed8fdf391d481f6
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-06-17 16:09:36 +0900

    test: 문자열 반대로 뒤집기
    StringBuilder에 있는 reverse() 메서드를 사용해서 str를 반대로 뒤집는다.

---
+++ src/lang/string/test/TestString10.java
@@ -0,0 +1,12 @@
+package lang.string.test;
+
+public class TestString10 {
+    public static void main(String[] args) {
+
+        String str = "Hello, Java";
+
+        System.out.println(new StringBuilder(str).reverse().toString());
+    }
+}
+
+// avaJ ,olleH
