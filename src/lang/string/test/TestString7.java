61091c7a3812f2d4f4436ce95e5d2ae0a40b60fe
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-06-17 15:58:37 +0900

    test: 단어 대체
    replace() 메서드를 사용해서 java라는 단어를 jvm으로 변경한다.

---
+++ src/lang/string/test/TestString7.java
@@ -0,0 +1,11 @@
+package lang.string.test;
+
+public class TestString7 {
+    public static void main(String[] args) {
+
+        String input = "hello java spring jpa java";
+        System.out.println(input.replace("java", "jvm"));
+    }
+}
+
+// hello jvm spring jpa jvm
