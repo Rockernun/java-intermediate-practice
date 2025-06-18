0768edc4f41a1b1d75e1a753fe109df885d18b97
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-06-18 14:09:55 +0900

    test: 박싱, 언박싱
    String을 Integer로, Integer를 int로, int를 Integer로 변환해서 출력한다. 오토 박싱, 오토 언박싱을 사용하지 말고 직접 변환한다.

---
+++ src/lang/wrapper/test/WrapperTest3.java
@@ -0,0 +1,27 @@
+package lang.wrapper.test;
+
+public class WrapperTest3 {
+    public static void main(String[] args) {
+
+        String str = "100";
+
+        // String에서 Integer로 변환
+        Integer integer1 = Integer.valueOf(str);
+        System.out.println("integer1 = " + integer1);
+
+        // Integer에서 int로 변환
+        int intValue = integer1.intValue();
+        System.out.println("intValue = " + intValue);
+
+        // int에서 Integer로 변환
+        Integer integer2 = Integer.valueOf(intValue);
+        System.out.println("integer2 = " + integer2);
+
+    }
+}
+
+/*
+integer1 = 100
+intValue = 100
+integer2 = 100
+*/
