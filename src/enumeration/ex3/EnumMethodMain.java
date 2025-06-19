74341812d950f96c28fa3388f0d5d45fdf422f58
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-06-19 13:22:48 +0900

    feat: enum values, name, ordinal, valueOf, toString 메서드를 활용한 열거형 주요 기능 학습 코드 작성

---
+++ src/enumeration/ex3/EnumMethodMain.java
@@ -0,0 +1,29 @@
+package enumeration.ex3;
+
+import java.util.Arrays;
+
+public class EnumMethodMain {
+    public static void main(String[] args) {
+
+        // 모든 ENUM 반환
+        Grade[] values = Grade.values();
+        System.out.println("values = " + Arrays.toString(values));
+
+        for (Grade value : values) {
+            System.out.println("name=" + value.name() + ", ordinal=" + value.ordinal());
+        }
+
+        // String -> ENUM 변환
+        String input = "GOLD";
+        Grade gold = Grade.valueOf(input);
+        System.out.println("gold = " + gold);
+    }
+}
+
+/*
+values = [BASIC, GOLD, DIAMOND]
+name=BASIC, ordinal=0
+name=GOLD, ordinal=1
+name=DIAMOND, ordinal=2
+gold = GOLD
+*/
