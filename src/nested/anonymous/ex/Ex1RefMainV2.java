825df9073ba33ac925a0ddceb8c9bce48853825f
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-06-26 16:06:53 +0900

    refactor: 지역 클래스를 활용해 실행 전략을 main 메서드 내에서 정의

---
+++ src/nested/anonymous/ex/Ex1RefMainV2.java
@@ -0,0 +1,50 @@
+package nested.anonymous.ex;
+
+import java.util.Random;
+
+public class Ex1RefMainV2 {
+
+    public static void hello(Process process) {
+
+        System.out.println("프로그램 시작!");
+        process.run();
+        System.out.println("프로그램 종료...");
+    }
+
+    public static void main(String[] args) {
+
+        class RollingDice implements Process {
+
+            @Override
+            public void run() {
+                int dice = new Random().nextInt(6) + 1;
+                System.out.println("주사위의 눈: " + dice);
+            }
+
+        }
+
+        class Sum implements Process {
+
+            @Override
+            public void run() {
+                for (int i = 1; i <= 3; i++) {
+                    System.out.println("i = " + i);
+                }
+            }
+        }
+
+        hello(new RollingDice());
+        hello(new Sum());
+    }
+}
+
+/*
+프로그램 시작!
+주사위의 눈: 2
+프로그램 종료...
+프로그램 시작!
+i = 1
+i = 2
+i = 3
+프로그램 종료...
+*/
