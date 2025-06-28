88d79c4e07270ff6377ebc88648f8a0b0dd3c7d9
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-06-28 12:33:27 +0900

    refactor: 네트워크 연결 및 전송 실패 시 조기 종료 처리 로직 추가

--- src/exception/ex1/MainV1.java
+++ src/exception/ex1/MainV1.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class MainV1 {
     public static void main(String[] args) {
 
-        NetworkServiceV1_1 networkService = new NetworkServiceV1_1();
+//        NetworkServiceV1_1 networkService = new NetworkServiceV1_1();
+        NetworkServiceV1_2 networkService = new NetworkServiceV1_2();
+
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
@@ -22,3 +24,22 @@ public class MainV1 {
         System.out.println("프로그램을 정상적으로 종료합니다.");
     }
 }
+
+/*
+전송할 데이터: hello
+http://example.com 서버가 성공적으로 연결되었습니다.
+http://example.com 서버에 데이터 전송: hello
+http://example.com 서버 연결이 해제되었습니다.
+
+전송할 데이터: error1
+http://example.com 서버 연결 실패!
+[네트워크 오류 발생] 오류 코드: connectError...
+
+전송할 데이터: error2
+http://example.com 서버가 성공적으로 연결되었습니다.
+http://example.com 서버에 데이터 전송 실패: error2
+[네트워크 오류 발생] 오류 코드: sendError...
+
+전송할 데이터: exit
+프로그램을 정상적으로 종료합니다.
+*/
