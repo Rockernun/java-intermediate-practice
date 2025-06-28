53e7ceaf7fda97feff71afabee1f176b8fbc6d13
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-06-28 12:36:21 +0900

    refactor: 네트워크 연결 실패 시에도 disconnect() 항상 호출하도록 수정

--- src/exception/ex1/MainV1.java
+++ src/exception/ex1/MainV1.java
@@ -6,7 +6,8 @@ public class MainV1 {
     public static void main(String[] args) {
 
 //        NetworkServiceV1_1 networkService = new NetworkServiceV1_1();
-        NetworkServiceV1_2 networkService = new NetworkServiceV1_2();
+//        NetworkServiceV1_2 networkService = new NetworkServiceV1_2();
+        NetworkServiceV1_3 networkService = new NetworkServiceV1_3();
 
 
         Scanner scanner = new Scanner(System.in);
@@ -34,11 +35,13 @@ http://example.com 서버 연결이 해제되었습니다.
 전송할 데이터: error1
 http://example.com 서버 연결 실패!
 [네트워크 오류 발생] 오류 코드: connectError...
+http://example.com 서버 연결이 해제되었습니다.
 
 전송할 데이터: error2
 http://example.com 서버가 성공적으로 연결되었습니다.
 http://example.com 서버에 데이터 전송 실패: error2
 [네트워크 오류 발생] 오류 코드: sendError...
+http://example.com 서버 연결이 해제되었습니다.
 
 전송할 데이터: exit
 프로그램을 정상적으로 종료합니다.
