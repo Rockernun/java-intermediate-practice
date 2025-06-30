53caba9835dadd56af2bc44ee50ebb29a79c48fc
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-06-30 17:22:01 +0900

    feat: try-with-resources 구문 도입 및 AutoCloseable 구현
    - NetworkServiceV5에서 try-with-resources 구문을 사용해 자원 정리를 명시적으로 처리하지 않아도 자동 정리되도록 개선

--- src/exception/ex4/MainV4.java
+++ src/exception/ex4/MainV4.java
@@ -7,7 +7,8 @@ import java.util.Scanner;
 public class MainV4 {
     public static void main(String[] args) {
 
-        NetworkServiceV4 networkService = new NetworkServiceV4();
+//        NetworkServiceV4 networkService = new NetworkServiceV4();
+        NetworkServiceV5 networkService = new NetworkServiceV5();
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
@@ -44,25 +45,36 @@ public class MainV4 {
 }
 
 /*
-전송할 데이터: error1
+전송할 데이터: hello
+http://example.com 서버가 성공적으로 연결되었습니다.
+http://example.com 서버에 데이터 전송: hello
+NetworkClientV5.close
 http://example.com 서버 연결이 해제되었습니다.
+
+전송할 데이터: error1
+NetworkClientV5.close
+http://example.com 서버 연결이 해제되었습니다.
+[예외 확인]: http://example.com 서버 연결 실패!
 사용자 메시지: 죄송합니다. 알 수 없는 문제가 발생했습니다!
 ==개발자용 디버깅 메시지==
 exception.ex4.exception.ConnectExceptionV4: http://example.com 서버 연결 실패!
-	at exception.ex4.NetworkClientV4.connect(NetworkClientV4.java:18)
-	at exception.ex4.NetworkServiceV4.sendMessage(NetworkServiceV4.java:13)
-	at exception.ex4.MainV4.main(MainV4.java:21)
+	at exception.ex4.NetworkClientV5.connect(NetworkClientV5.java:18)
+	at exception.ex4.NetworkServiceV5.sendMessage(NetworkServiceV5.java:11)
+	at exception.ex4.MainV4.main(MainV4.java:22)
 
 전송할 데이터: error2
 http://example.com 서버가 성공적으로 연결되었습니다.
+NetworkClientV5.close
 http://example.com 서버 연결이 해제되었습니다.
+[예외 확인]: http://example.com 서버에 데이터 전송 실패: error2
 사용자 메시지: 죄송합니다. 알 수 없는 문제가 발생했습니다!
 ==개발자용 디버깅 메시지==
 exception.ex4.exception.SendExceptionV4: http://example.com 서버에 데이터 전송 실패: error2
-	at exception.ex4.NetworkClientV4.send(NetworkClientV4.java:26)
-	at exception.ex4.NetworkServiceV4.sendMessage(NetworkServiceV4.java:14)
-	at exception.ex4.MainV4.main(MainV4.java:21)
+	at exception.ex4.NetworkClientV5.send(NetworkClientV5.java:26)
+	at exception.ex4.NetworkServiceV5.sendMessage(NetworkServiceV5.java:12)
+	at exception.ex4.MainV4.main(MainV4.java:22)
 [전송 오류] 전송 데이터: error2
 
-전송할 데이터:
+전송할 데이터: exit
+프로그램을 정상적으로 종료합니다.
 */
