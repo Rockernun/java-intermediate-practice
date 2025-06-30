b835c301b97f44cfb97162267ef2b63753f8488d
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-06-30 10:18:23 +0900

    test: disconnect()를 try-catch 외부로 이동시켜 자원 정리 누락 상황 실험
    - NetworkClientV2.send() 내부에 RuntimeException을 강제로 발생시키는 코드 추가하여,
    try-catch 블록 외부에 있는 disconnect()가 호출되지 않음을 확인.
    
    - error1 → connect 실패 → catch 처리 → disconnect() 정상 호출
    - error2 → send 중 RuntimeException → catch되지 않아 disconnect() 호출 누락 발생
    
    - finally 블록을 도입하여 자원 정리 보장 필요

--- src/exception/ex2/MainV2.java
+++ src/exception/ex2/MainV2.java
@@ -7,7 +7,8 @@ public class MainV2 {
 
 //        NetworkServiceV2_1 networkService = new NetworkServiceV2_1();
 //        NetworkServiceV2_2 networkService = new NetworkServiceV2_2();
-        NetworkServiceV2_3 networkService = new NetworkServiceV2_3();
+//        NetworkServiceV2_3 networkService = new NetworkServiceV2_3();
+        NetworkServiceV2_4 networkService = new NetworkServiceV2_4();
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
@@ -26,18 +27,14 @@ public class MainV2 {
 }
 
 /*
-전송할 데이터: hello
-http://example.com 서버가 성공적으로 연결되었습니다.
-http://example.com 서버에 데이터 전송: hello
-http://example.com 서버 연결이 해제되었습니다.
-
 전송할 데이터: error1
 [오류] 코드: connectError..., 메시지: http://example.com 서버 연결 실패!
+http://example.com 서버 연결이 해제되었습니다.
 
 전송할 데이터: error2
 http://example.com 서버가 성공적으로 연결되었습니다.
-[오류] 코드: sendError..., 메시지: http://example.com 서버에 데이터 전송 실패: error2
-
-전송할 데이터: exit
-프로그램을 정상적으로 종료합니다.
+Exception in thread "main" java.lang.RuntimeException: ex
+	at exception.ex2.NetworkClientV2.send(NetworkClientV2.java:26)
+	at exception.ex2.NetworkServiceV2_4.sendMessage(NetworkServiceV2_4.java:13)
+	at exception.ex2.MainV2.main(MainV2.java:21)
 */
