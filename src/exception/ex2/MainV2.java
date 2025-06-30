0da85e5ca48782fff645312157e48ea2ea89ee94
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-06-30 09:53:10 +0900

    refactor: 예외 복구를 위한 try-catch 분리 및 예외 메시지 출력 로직 추가
    - 하지만 현재 정상 흐름 로직과 예외 처리 로직이 혼재되어 있다.
    - 사용 후에 반드시 disconnect() 메서드를 호출해줘야 하는데 그렇지 않고 있다.

--- src/exception/ex2/MainV2.java
+++ src/exception/ex2/MainV2.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class MainV2 {
     public static void main(String[] args) throws NetworkClientExceptionV2 {
 
-        NetworkServiceV2_1 networkService = new NetworkServiceV2_1();
+//        NetworkServiceV2_1 networkService = new NetworkServiceV2_1();
+        NetworkServiceV2_2 networkService = new NetworkServiceV2_2();
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
@@ -22,3 +23,20 @@ public class MainV2 {
         System.out.println("프로그램을 정상적으로 종료합니다.");
     }
 }
+
+/*
+전송할 데이터: hello
+http://example.com 서버가 성공적으로 연결되었습니다.
+http://example.com 서버에 데이터 전송: hello
+http://example.com 서버 연결이 해제되었습니다.
+
+전송할 데이터: error1
+[오류] 코드: connectError..., 메시지: http://example.com 서버 연결 실패!
+
+전송할 데이터: error2
+http://example.com 서버가 성공적으로 연결되었습니다.
+[오류] 코드: sendError..., 메시지: http://example.com 서버에 데이터 전송 실패: error2
+
+전송할 데이터: exit
+프로그램을 정상적으로 종료합니다.
+*/
