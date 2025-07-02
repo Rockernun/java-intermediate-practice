6eb2d48c12e2fd21041f13ee27e40b6c902fff1e
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-07-02 09:09:14 +0900

    feat: Integer, String 타입을 담고 출력하기
    - 향후 Double, Boolean 등 다양한 데이터 타입이 도입될 경우 중복이 발생한다.
    - Object 클래스를 이용해서 다형성을 구현하도록 한다.

---
+++ src/generic/ex1/BoxMain1.java
@@ -0,0 +1,21 @@
+package generic.ex1;
+
+public class BoxMain1 {
+    public static void main(String[] args) {
+
+        IntegerBox integerBox = new IntegerBox();
+        integerBox.setValue(10);
+        Integer integer = integerBox.getValue();
+        System.out.println("integer = " + integer);
+
+        StringBox stringBox = new StringBox();
+        stringBox.setValue("hello");
+        String string = stringBox.getValue();
+        System.out.println("string = " + string);
+    }
+}
+
+/*
+integer = 10
+string = hello
+*/
