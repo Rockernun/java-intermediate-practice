a62e857df22a6dba796ed2b7cf693b495cb0f6a0
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-07-05 13:05:44 +0900

    fix
    컴파일 오류 나는 파일 주석 처리

--- src/lang/object/tostring/BadObjectPrinter.java
+++ src/lang/object/tostring/BadObjectPrinter.java
@@ -1,13 +1,13 @@
-package lang.object.tostring;
-
-public class BadObjectPrinter {
-    public static void print(Car car) {
-        String string = car.carInfo();
-        System.out.println(string);
-    }
-
-    public static void print(Dog dog) {
-        String string = dog.dogInfo();
-        System.out.println(string);
-    }
-}
+//package lang.object.tostring;
+//
+//public class BadObjectPrinter {
+//    public static void print(Car car) {
+//        String string = car.carInfo();
+//        System.out.println(string);
+//    }
+//
+//    public static void print(Dog dog) {
+//        String string = dog.dogInfo();
+//        System.out.println(string);
+//    }
+//}
