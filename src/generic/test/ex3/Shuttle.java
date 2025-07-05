038d568614ee65b1ddde8e707e224ac994cdf68e
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-07-05 13:34:11 +0900

    test: 제네릭 메서드와 와일드 카드 사용
    - UnitPrinter 클래스를 만든다.
      - UnitPrinter.printV1(): 제네릭 메서드로 구현한다.
      - UnitPrinter.printV2(): 와일드 카드로 구현한다.
      - 위 두 메서드는 Shuttle에 들어있는 유닛의 정보를 출력한다.

--- src/generic/test/ex3/Shuttle.java
+++ src/generic/test/ex3/Shuttle.java
@@ -10,6 +10,10 @@ public class Shuttle<T extends BioUnit> {
         this.unit = unit;
     }
 
+    public T out() {
+        return unit;
+    }
+
     public void showInfo() {
         System.out.println("이름: " + unit.getName() + ", HP: " + unit.getHp());
     }
