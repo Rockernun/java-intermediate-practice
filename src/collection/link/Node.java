f902ddf2495d4379614a0fa59a3a0459cf12efbd
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-07-09 15:01:21 +0900

    refactor: Node 클래스에 연결 형식의 toString() 메서드 구현
    - Node 클래스의 toString() 메서드를 재정의하여 연결된 노드들을 "[A -> B -> C]" 형태로 출력하도록 구현

--- src/collection/link/Node.java
+++ src/collection/link/Node.java
@@ -8,4 +8,30 @@ public class Node {
     public Node(Object item) {
         this.item = item;
     }
+
+//    // IDE 생성 toString()
+//    @Override
+//    public String toString() {
+//        return "Node{" +
+//                "item=" + item +
+//                ", next=" + next +
+//                '}';
+//    }
+
+    // [A -> B -> C] 형식으로 출력하고 싶다.
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Node x = this;
+        sb.append("[");
+        while (x != null) {
+            sb.append(x.item);
+            if (x.next != null) {
+                sb.append(" -> ");
+            }
+            x = x.next;
+        }
+        sb.append("]");
+        return sb.toString();
+    }
 }
