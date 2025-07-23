a87608737cb0eb488779a127477d78a60617e337
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-07-23 20:04:28 +0900

    feat: 반복자(MyArrayIterator) 구현으로 배열 순회
    - MyArrayIterator 클래스: Iterator<Integer>를 구현하여 int 배열을 순회할 수 있도록 정의
    - MyArray 클래스: Iterable<Integer> 인터페이스를 구현하여 for-each 루프나 iterator 사용 가능하도록 처리
    - MyArrayMain에서 사용자 정의 반복자를 활용한 순회

---
+++ src/collection/iterable/MyArrayMain.java
@@ -0,0 +1,25 @@
+package collection.iterable;
+
+import java.util.Iterator;
+
+public class MyArrayMain {
+    public static void main(String[] args) {
+
+        MyArray myArr = new MyArray(new int[]{1, 2, 3, 4});
+
+        Iterator<Integer> iterator = myArr.iterator();
+        System.out.println("iterator 사용...");
+        while (iterator.hasNext()) {
+            Integer value = iterator.next();
+            System.out.println("value = " + value);
+        }
+    }
+}
+
+/*
+iterator 사용...
+value = 1
+value = 2
+value = 3
+value = 4
+*/
